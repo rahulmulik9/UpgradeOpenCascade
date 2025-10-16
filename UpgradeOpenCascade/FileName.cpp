6ae647f984e54915287850db1fefb0727966d39f
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2025-10-16 14:57:11 +0530

    cc

--- UpgradeOpenCascade/FileName.cpp
+++ UpgradeOpenCascade/FileName.cpp
@@ -1,4 +1,4 @@
-// Created on: 2007-07-17
+// Created on: 2007-08-01
 // Created by: Alexander GRIGORIEV
 // Copyright (c) 2007-2014 OPEN CASCADE SAS
 //
@@ -13,13 +13,11 @@
 // Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
-#include <VrmlData_Material.hxx>
-#include <Precision.hxx>
-#include <VrmlData_InBuffer.hxx>
+#include <VrmlData_WorldInfo.hxx>
 #include <VrmlData_Scene.hxx>
-#include <gp_XYZ.hxx>
+#include <VrmlData_InBuffer.hxx>
 
-IMPLEMENT_STANDARD_RTTIEXT(VrmlData_Material, VrmlData_Node)
+IMPLEMENT_STANDARD_RTTIEXT(VrmlData_WorldInfo, VrmlData_Node)
 
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_DEPRECATE
@@ -28,217 +26,149 @@ IMPLEMENT_STANDARD_RTTIEXT(VrmlData_Material, VrmlData_Node)
 
 
 //=======================================================================
-//function : VrmlData_Material()
-//purpose  : Empty Constructor
-//=======================================================================
-
-VrmlData_Material::VrmlData_Material()
-    : myAmbientIntensity(0.2),
-    myShininess(0.2),
-    myTransparency(0.),
-    myDiffuseColor(0.8, 0.8, 0.8, Quantity_TOC_sRGB),
-    myEmissiveColor(Quantity_NOC_BLACK),
-    mySpecularColor(Quantity_NOC_BLACK)
-{
-}
-
-//=======================================================================
-//function : VrmlData_Material
+//function : VrmlData_WorldInfo::VrmlData_WorldInfo
 //purpose  : Constructor
 //=======================================================================
 
-VrmlData_Material::VrmlData_Material(const VrmlData_Scene& theScene,
+VrmlData_WorldInfo::VrmlData_WorldInfo(const VrmlData_Scene& theScene,
     const char* theName,
-    const Standard_Real    theAmbientIntens,
-    const Standard_Real    theShininess,
-    const Standard_Real    theTransparency)
+    const char* theTitle)
     : VrmlData_Node(theScene, theName),
-    myAmbientIntensity(theAmbientIntens < 0. ? 0.2 : theAmbientIntens),
-    myShininess(theShininess < 0. ? 0.2 : theShininess),
-    myTransparency(theTransparency < 0 ? 0. : theTransparency),
-    myDiffuseColor(0.8, 0.8, 0.8, Quantity_TOC_sRGB),
-    myEmissiveColor(Quantity_NOC_BLACK),
-    mySpecularColor(Quantity_NOC_BLACK)
+    myInfo(theScene.Allocator())
 {
+    SetTitle(theTitle);
 }
 
+//=======================================================================
+//function : SetTitle
+//purpose  : Set or modify the title.
+//=======================================================================
+
+void VrmlData_WorldInfo::SetTitle(const char* theString)
+{
+    if (theString == 0L)
+        myTitle = 0L;
+    else {
+        const size_t len = strlen(theString) + 1;
+        if (len == 1)
+            myTitle = 0L;
+        else {
+            myTitle = static_cast <const char*>(Scene().Allocator()->Allocate(len));
+            memcpy(const_cast<char*> (myTitle), theString, len);
+        }
+    }
+}
 
 //=======================================================================
-//function : VrmlData_Material::Clone
-//purpose  : 
+//function : AddInfo
+//purpose  : Add a string to the list of info strings.
 //=======================================================================
 
-Handle(VrmlData_Node) VrmlData_Material::Clone
+void VrmlData_WorldInfo::AddInfo(const char* theString)
+{
+    if (theString != 0L)
+        if (*theString != '\0') {
+            const size_t len = strlen(theString) + 1;
+            char* aStr = static_cast <char*>(Scene().Allocator()->Allocate(len));
+            memcpy(aStr, theString, len);
+            myInfo.Append(aStr);
+        }
+}
+
+//=======================================================================
+//function : Clone
+//purpose  : Create a copy of this node
+//=======================================================================
+
+Handle(VrmlData_Node) VrmlData_WorldInfo::Clone
 (const Handle(VrmlData_Node)& theOther) const
 {
-    Handle(VrmlData_Material) aResult =
-        Handle(VrmlData_Material)::DownCast(VrmlData_Node::Clone(theOther));
+    Handle(VrmlData_WorldInfo) aResult =
+        Handle(VrmlData_WorldInfo)::DownCast(VrmlData_Node::Clone(theOther));
     if (aResult.IsNull())
         aResult =
-        new VrmlData_Material(theOther.IsNull() ? Scene() : theOther->Scene(),
+        new VrmlData_WorldInfo(theOther.IsNull() ? Scene() : theOther->Scene(),
             Name());
 
-    aResult->SetAmbientIntensity(myAmbientIntensity);
-    aResult->SetShininess(myShininess);
-    aResult->SetTransparency(myTransparency);
-    aResult->SetDiffuseColor(myDiffuseColor);
-    aResult->SetEmissiveColor(myEmissiveColor);
-    aResult->SetSpecularColor(mySpecularColor);
+    if (&aResult->Scene() == &Scene()) {
+        aResult->myTitle = myTitle;
+        aResult->myInfo = myInfo;
+    }
+    else {
+        aResult->SetTitle(myTitle);
+        NCollection_List <const char*>::Iterator anIter(myInfo);
+        for (; anIter.More(); anIter.Next())
+            aResult->AddInfo(anIter.Value());
+    }
     return aResult;
 }
 
 //=======================================================================
-//function : VrmlData_Material::Read
-//purpose  : 
+//function : Read
+//purpose  : Read the Node from input stream.
 //=======================================================================
 
-VrmlData_ErrorStatus VrmlData_Material::Read(VrmlData_InBuffer& theBuffer)
+VrmlData_ErrorStatus VrmlData_WorldInfo::Read(VrmlData_InBuffer& theBuffer)
 {
     VrmlData_ErrorStatus aStatus;
-    const Standard_Real aConf = 0.001 * Precision::Confusion();
-    Standard_Real anIntensity[3] = { 0.2, 0.2, 0. };
-    gp_XYZ        aColor[3] = {
-      gp_XYZ(0.8, 0.8, 0.8),
-      gp_XYZ(0.0, 0.0, 0.0),
-      gp_XYZ(0.0, 0.0, 0.0)
-    };
     while (OK(aStatus, VrmlData_Scene::ReadLine(theBuffer))) {
-        if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "ambientIntensity")) {
-            if (OK(aStatus, Scene().ReadReal(theBuffer, anIntensity[0],
-                Standard_False, Standard_False)))
-                if (anIntensity[0] < -aConf || anIntensity[0] > 1. + aConf) {
-                    aStatus = VrmlData_IrrelevantNumber;
-                    break;
-                }
+
+        if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "title")) {
+            TCollection_AsciiString aTitleString;
+            if (OK(aStatus, ReadString(theBuffer, aTitleString)))
+                SetTitle(aTitleString.ToCString());
+
         }
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "shininess")) {
-            if (OK(aStatus, Scene().ReadReal(theBuffer, anIntensity[1],
-                Standard_False, Standard_False)))
-                if (anIntensity[1] < -aConf || anIntensity[1] > 1. + aConf) {
-                    aStatus = VrmlData_IrrelevantNumber;
-                    break;
-                }
-        }
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "transparency")) {
-            if (OK(aStatus, Scene().ReadReal(theBuffer, anIntensity[2],
-                Standard_False, Standard_False)))
-                if (anIntensity[2] < -aConf || anIntensity[2] > 1. + aConf) {
-                    aStatus = VrmlData_IrrelevantNumber;
-                    break;
-                }
-        }
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "diffuseColor")) {
-            if (OK(aStatus, Scene().ReadXYZ(theBuffer, aColor[0],
-                Standard_False, Standard_False)))
-                if (aColor[0].X() < -aConf || aColor[0].X() > 1. + aConf ||
-                    aColor[0].Y() < -aConf || aColor[0].Y() > 1. + aConf ||
-                    aColor[0].Z() < -aConf || aColor[0].Z() > 1. + aConf)
-                {
-                    aStatus = VrmlData_IrrelevantNumber;
-                    break;
-                }
-        }
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "emissiveColor")) {
-            if (OK(aStatus, Scene().ReadXYZ(theBuffer, aColor[1],
-                Standard_False, Standard_False)))
-                if (aColor[1].X() < -aConf || aColor[1].X() > 1. + aConf ||
-                    aColor[1].Y() < -aConf || aColor[1].Y() > 1. + aConf ||
-                    aColor[1].Z() < -aConf || aColor[1].Z() > 1. + aConf)
-                {
-                    aStatus = VrmlData_IrrelevantNumber;
-                    break;
-                }
-        }
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "specularColor")) {
-            if (OK(aStatus, Scene().ReadXYZ(theBuffer, aColor[2],
-                Standard_False, Standard_False)))
-                if (aColor[2].X() < -aConf || aColor[2].X() > 1. + aConf ||
-                    aColor[2].Y() < -aConf || aColor[2].Y() > 1. + aConf ||
-                    aColor[2].Z() < -aConf || aColor[2].Z() > 1. + aConf)
-                {
-                    aStatus = VrmlData_IrrelevantNumber;
-                    break;
-                }
+        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "info")) {
+            NCollection_List<TCollection_AsciiString> lstInfo;
+            if (OK(aStatus, ReadMultiString(theBuffer, lstInfo))) {
+                NCollection_List<TCollection_AsciiString>::Iterator anIter(lstInfo);
+                for (; anIter.More(); anIter.Next())
+                    AddInfo(anIter.Value().ToCString());
+            }
         }
         else
-            break;
-
-        if (!OK(aStatus))
             break;
     }
 
     // Read the terminating (closing) brace
     if (OK(aStatus))
         aStatus = readBrace(theBuffer);
-
-    // Store the values in the Material node instance
-    if (OK(aStatus)) {
-        myAmbientIntensity = anIntensity[0];
-        myShininess = anIntensity[1];
-        myTransparency = anIntensity[2];
-        myDiffuseColor.SetValues(aColor[0].X(), aColor[0].Y(), aColor[0].Z(),
-            Quantity_TOC_sRGB);
-        myEmissiveColor.SetValues(aColor[1].X(), aColor[1].Y(), aColor[1].Z(),
-            Quantity_TOC_sRGB);
-        mySpecularColor.SetValues(aColor[2].X(), aColor[2].Y(), aColor[2].Z(),
-            Quantity_TOC_sRGB);
-    }
     return aStatus;
 }
 
 //=======================================================================
-//function : VrmlData_Material::Write
-//purpose  : 
+//function : Write
+//purpose  : Write the Node to the Scene output.
 //=======================================================================
 
-VrmlData_ErrorStatus VrmlData_Material::Write(const char* thePrefix) const
+VrmlData_ErrorStatus VrmlData_WorldInfo::Write(const char* thePrefix) const
 {
-    VrmlData_ErrorStatus aStatus = VrmlData_StatusOK;
+    VrmlData_ErrorStatus aStatus(VrmlData_StatusOK);
     const VrmlData_Scene& aScene = Scene();
-    static char header[] = "Material {";
+    static char header[] = "WorldInfo {";
     if (aScene.IsDummyWrite() == Standard_False &&
         OK(aStatus, aScene.WriteLine(thePrefix, header, GlobalIndent())))
     {
-        char buf[128];
-        Standard_Real val[3];
-        const Quantity_TypeOfColor bidType = Quantity_TOC_sRGB;
-        const Standard_Real aConf(0.001 * Precision::Confusion());
+        char buf[4096];
+        if (myTitle) {
+            Sprintf(buf, "title \"%s\"", myTitle);
+            aStatus = aScene.WriteLine(buf);
+        }
 
-        if (OK(aStatus) && fabs(myAmbientIntensity - 0.2) > aConf) {
-            Sprintf(buf, "%.6g", myAmbientIntensity);
-            aStatus = aScene.WriteLine("ambientIntensity ", buf);
-        }
-        if (OK(aStatus)) {
-            myDiffuseColor.Values(val[0], val[1], val[2], bidType);
-            if ((val[0] - 0.8) * (val[0] - 0.8) +
-                (val[1] - 0.8) * (val[1] - 0.8) +
-                (val[2] - 0.8) * (val[2] - 0.8) > 1e-7)
-            {
-                Sprintf(buf, "%.6g %.6g %.6g", val[0], val[1], val[2]);
-                aStatus = aScene.WriteLine("diffuseColor     ", buf);
+        if (myInfo.IsEmpty() == Standard_False && OK(aStatus)) {
+            if (OK(aStatus, aScene.WriteLine("info [", 0L, GlobalIndent()))) {
+                NCollection_List<const char*>::Iterator anIter(myInfo);
+                while (anIter.More()) {
+                    Sprintf(buf, "\"%s\"", anIter.Value());
+                    anIter.Next();
+                    if (anIter.More())
+                        aStatus = aScene.WriteLine(buf, ",");
+                    else
+                        aStatus = aScene.WriteLine(buf);
+                }
             }
-        }
-        if (OK(aStatus)) {
-            myEmissiveColor.Values(val[0], val[1], val[2], bidType);
-            if (val[0] * val[0] + val[1] * val[1] + val[2] * val[2] > 1e-7) {
-                Sprintf(buf, "%.6g %.6g %.6g", val[0], val[1], val[2]);
-                aStatus = aScene.WriteLine("emissiveColor    ", buf);
-            }
-        }
-        if (OK(aStatus) && fabs(myShininess - 0.2) > aConf) {
-            Sprintf(buf, "%.6g", myShininess);
-            aStatus = aScene.WriteLine("shininess        ", buf);
-        }
-        if (OK(aStatus)) {
-            mySpecularColor.Values(val[0], val[1], val[2], bidType);
-            if (val[0] * val[0] + val[1] * val[1] + val[2] * val[2] > 1e-7) {
-                Sprintf(buf, "%.6g %.6g %.6g", val[0], val[1], val[2]);
-                aStatus = aScene.WriteLine("specularColor    ", buf);
-            }
-        }
-        if (OK(aStatus) && myTransparency > aConf) {
-            Sprintf(buf, "%.6g", myTransparency);
-            aStatus = aScene.WriteLine("transparency     ", buf);
+            aStatus = aScene.WriteLine("]", 0L, -GlobalIndent());
         }
 
         aStatus = WriteClosing();
@@ -251,30 +181,7 @@ VrmlData_ErrorStatus VrmlData_Material::Write(const char* thePrefix) const
 //purpose  : 
 //=======================================================================
 
-Standard_Boolean VrmlData_Material::IsDefault() const
+Standard_Boolean VrmlData_WorldInfo::IsDefault() const
 {
-    const Standard_Real aConf(0.001 * Precision::Confusion());
-    Standard_Boolean aResult(Standard_False);
-    if (fabs(myAmbientIntensity - 0.2) < aConf &&
-        fabs(myShininess - 0.2) < aConf &&
-        myTransparency < aConf)
-    {
-        Standard_Real val[3][3];
-        const Quantity_TypeOfColor bidType = Quantity_TOC_sRGB;
-        myDiffuseColor.Values(val[0][0], val[0][1], val[0][2], bidType);
-        myEmissiveColor.Values(val[1][0], val[1][1], val[1][2], bidType);
-        mySpecularColor.Values(val[2][0], val[2][1], val[2][2], bidType);
-        aResult = (((val[0][0] - 0.8) * (val[0][0] - 0.8) +
-            (val[0][1] - 0.8) * (val[0][1] - 0.8) +
-            (val[0][2] - 0.8) * (val[0][2] - 0.8) < 1e-7) &&
-            (val[1][0] * val[1][0] +
-                val[1][1] * val[1][0] +
-                val[1][2] * val[1][0] < 1e-7) &&
-            (val[2][0] * val[2][0] +
-                val[2][1] * val[2][0] +
-                val[2][2] * val[2][0] < 1e-7));
-    }
-    return aResult;
+    return (myTitle == 0L && myInfo.IsEmpty());
 }
-
-
