80816cc833b367d3b521885dc23c81ea4890e5d4
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2025-10-28 14:59:46 +0530

    VrmlData_Material

--- UpgradeOpenCascade/FileName.cpp
+++ UpgradeOpenCascade/FileName.cpp
@@ -1,4 +1,4 @@
-// Created on: 2007-08-01
+// Created on: 2007-07-17
 // Created by: Alexander GRIGORIEV
 // Copyright (c) 2007-2014 OPEN CASCADE SAS
 //
@@ -13,20 +13,13 @@
 // Alternatively, this file may be used under the terms of Open CASCADE
 // commercial license or contractual agreement.
 
-#include <VrmlData_IndexedLineSet.hxx>
-#include <VrmlData_Scene.hxx>
+#include <VrmlData_Material.hxx>
+#include <Precision.hxx>
 #include <VrmlData_InBuffer.hxx>
-#include <VrmlData_UnknownNode.hxx>
-#include <BRep_Builder.hxx>
-#include <Poly_Polygon3D.hxx>
-#include <TColStd_Array1OfReal.hxx>
-#include <TColgp_Array1OfPnt.hxx>
-#include <TopoDS_Edge.hxx>
-#include <TopoDS_Wire.hxx>
-#include <VrmlData_Color.hxx>
-#include <VrmlData_Coordinate.hxx>
+#include <VrmlData_Scene.hxx>
+#include <gp_XYZ.hxx>
 
-IMPLEMENT_STANDARD_RTTIEXT(VrmlData_IndexedLineSet, VrmlData_Geometry)
+IMPLEMENT_STANDARD_RTTIEXT(VrmlData_Material, VrmlData_Node)
 
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_DEPRECATE
@@ -35,167 +28,218 @@ IMPLEMENT_STANDARD_RTTIEXT(VrmlData_IndexedLineSet, VrmlData_Geometry)
 
 
 //=======================================================================
-//function : GetColor
+//function : VrmlData_Material()
+//purpose  : Empty Constructor
+//=======================================================================
+
+VrmlData_Material::VrmlData_Material()
+    : myAmbientIntensity(0.2),
+    myShininess(0.2),
+    myTransparency(0.),
+    myDiffuseColor(0.8, 0.8, 0.8, Quantity_TOC_sRGB),
+    myEmissiveColor(Quantity_NOC_BLACK),
+    mySpecularColor(Quantity_NOC_BLACK)
+{
+}
+
+//=======================================================================
+//function : VrmlData_Material
+//purpose  : Constructor
+//=======================================================================
+
+VrmlData_Material::VrmlData_Material(const VrmlData_Scene& theScene,
+    const char* theName,
+    const Standard_Real    theAmbientIntens,
+    const Standard_Real    theShininess,
+    const Standard_Real    theTransparency)
+    : VrmlData_Node(theScene, theName),
+    myAmbientIntensity(theAmbientIntens < 0. ? 0.2 : theAmbientIntens),
+    myShininess(theShininess < 0. ? 0.2 : theShininess),
+    myTransparency(theTransparency < 0 ? 0. : theTransparency),
+    myDiffuseColor(0.8, 0.8, 0.8, Quantity_TOC_sRGB),
+    myEmissiveColor(Quantity_NOC_BLACK),
+    mySpecularColor(Quantity_NOC_BLACK)
+{
+}
+
+
+//=======================================================================
+//function : VrmlData_Material::Clone
 //purpose  : 
 //=======================================================================
 
-Quantity_Color VrmlData_IndexedLineSet::GetColor
-(const Standard_Integer /*iFace*/,
-    const Standard_Integer /*iVertex*/)
-{
-    //TODO
-    return Quantity_NOC_BLACK;
-}
-
-//=======================================================================
-//function : TShape
-//purpose  : Query the shape. This method checks the flag myIsModified;
-//           if True it should rebuild the shape presentation.
-//=======================================================================
-
-const Handle(TopoDS_TShape)& VrmlData_IndexedLineSet::TShape()
-{
-    if (myNbPolygons == 0)
-        myTShape.Nullify();
-    else if (myIsModified) {
-        Standard_Integer i;
-        BRep_Builder aBuilder;
-        const gp_XYZ* arrNodes = myCoords->Values();
-
-        // Create the Wire
-        TopoDS_Wire aWire;
-        aBuilder.MakeWire(aWire);
-        for (i = 0; i < (int)myNbPolygons; i++) {
-            const Standard_Integer* arrIndice;
-            const Standard_Integer nNodes = Polygon(i, arrIndice);
-            TColgp_Array1OfPnt   arrPoint(1, nNodes);
-            TColStd_Array1OfReal arrParam(1, nNodes);
-            for (Standard_Integer j = 0; j < nNodes; j++) {
-                arrPoint(j + 1).SetXYZ(arrNodes[arrIndice[j]]);
-                arrParam(j + 1) = j;
-            }
-            const Handle(Poly_Polygon3D) aPolyPolygon =
-                new Poly_Polygon3D(arrPoint, arrParam);
-            TopoDS_Edge anEdge;
-            aBuilder.MakeEdge(anEdge, aPolyPolygon);
-            aBuilder.Add(aWire, anEdge);
-        }
-        myTShape = aWire.TShape();
-    }
-    return myTShape;
-}
-
-//=======================================================================
-//function : Clone
-//purpose  : Create a copy of this node
-//=======================================================================
-
-Handle(VrmlData_Node) VrmlData_IndexedLineSet::Clone
+Handle(VrmlData_Node) VrmlData_Material::Clone
 (const Handle(VrmlData_Node)& theOther) const
 {
-    Handle(VrmlData_IndexedLineSet) aResult =
-        Handle(VrmlData_IndexedLineSet)::DownCast(VrmlData_Node::Clone(theOther));
+    Handle(VrmlData_Material) aResult =
+        Handle(VrmlData_Material)::DownCast(VrmlData_Node::Clone(theOther));
     if (aResult.IsNull())
         aResult =
-        new VrmlData_IndexedLineSet(theOther.IsNull() ? Scene() : theOther->Scene(),
+        new VrmlData_Material(theOther.IsNull() ? Scene() : theOther->Scene(),
             Name());
 
-    if (&aResult->Scene() == &Scene()) {
-        aResult->SetCoordinates(myCoords);
-        aResult->SetColors(myColors);
-        aResult->SetPolygons(myNbPolygons, myArrPolygons);
-        aResult->SetColorInd(myNbColors, myArrColorInd);
-    }
-    else {
-        // Create a dummy node to pass the different Scene instance to methods Clone
-        const Handle(VrmlData_UnknownNode) aDummyNode =
-            new VrmlData_UnknownNode(aResult->Scene());
-        if (myCoords.IsNull() == Standard_False)
-            aResult->SetCoordinates(Handle(VrmlData_Coordinate)::DownCast
-            (myCoords->Clone(aDummyNode)));
-        if (myColors.IsNull() == Standard_False)
-            aResult->SetColors(Handle(VrmlData_Color)::DownCast
-            (myColors->Clone(aDummyNode)));
-        //TODO: Replace the following lines with the relevant copying
-        aResult->SetPolygons(myNbPolygons, myArrPolygons);
-        aResult->SetColorInd(myNbColors, myArrColorInd);
-    }
-    aResult->SetColorPerVertex(myColorPerVertex);
+    aResult->SetAmbientIntensity(myAmbientIntensity);
+    aResult->SetShininess(myShininess);
+    aResult->SetTransparency(myTransparency);
+    aResult->SetDiffuseColor(myDiffuseColor);
+    aResult->SetEmissiveColor(myEmissiveColor);
+    aResult->SetSpecularColor(mySpecularColor);
     return aResult;
 }
 
 //=======================================================================
-//function : Read
-//purpose  : Read the Node from input stream.
+//function : VrmlData_Material::Read
+//purpose  : 
 //=======================================================================
 
-VrmlData_ErrorStatus VrmlData_IndexedLineSet::Read
-(VrmlData_InBuffer& theBuffer)
+VrmlData_ErrorStatus VrmlData_Material::Read(VrmlData_InBuffer& theBuffer)
 {
     VrmlData_ErrorStatus aStatus;
-    const VrmlData_Scene& aScene = Scene();
-    while (OK(aStatus, VrmlData_Scene::ReadLine(theBuffer)))
-    {
-        if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "colorPerVertex"))
-            aStatus = ReadBoolean(theBuffer, myColorPerVertex);
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "coordIndex"))
-            aStatus = aScene.ReadArrIndex(theBuffer, myArrPolygons, myNbPolygons);
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "colorIndex"))
-            aStatus = aScene.ReadArrIndex(theBuffer, myArrColorInd, myNbColors);
-        // These two checks should be the last one to avoid their interference
-        // with the other tokens (e.g., coordIndex)
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "color"))
-        {
-            Handle(VrmlData_Node) aNode;
-            aStatus = ReadNode(theBuffer, aNode,
-                STANDARD_TYPE(VrmlData_Color));
-            myColors = Handle(VrmlData_Color)::DownCast(aNode);
+    const Standard_Real aConf = 0.001 * Precision::Confusion();
+    Standard_Real anIntensity[3] = { 0.2, 0.2, 0. };
+    gp_XYZ        aColor[3] = {
+      gp_XYZ(0.8, 0.8, 0.8),
+      gp_XYZ(0.0, 0.0, 0.0),
+      gp_XYZ(0.0, 0.0, 0.0)
+    };
+    while (OK(aStatus, VrmlData_Scene::ReadLine(theBuffer))) {
+        if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "ambientIntensity")) {
+            if (OK(aStatus, Scene().ReadReal(theBuffer, anIntensity[0],
+                Standard_False, Standard_False)))
+                if (anIntensity[0] < -aConf || anIntensity[0] > 1. + aConf) {
+                    aStatus = VrmlData_IrrelevantNumber;
+                    break;
+                }
         }
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "coord"))
-        {
-            Handle(VrmlData_Node) aNode;
-            aStatus = ReadNode(theBuffer, aNode,
-                STANDARD_TYPE(VrmlData_Coordinate));
-            myCoords = Handle(VrmlData_Coordinate)::DownCast(aNode);
+        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "shininess")) {
+            if (OK(aStatus, Scene().ReadReal(theBuffer, anIntensity[1],
+                Standard_False, Standard_False)))
+                if (anIntensity[1] < -aConf || anIntensity[1] > 1. + aConf) {
+                    aStatus = VrmlData_IrrelevantNumber;
+                    break;
+                }
+        }
+        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "transparency")) {
+            if (OK(aStatus, Scene().ReadReal(theBuffer, anIntensity[2],
+                Standard_False, Standard_False)))
+                if (anIntensity[2] < -aConf || anIntensity[2] > 1. + aConf) {
+                    aStatus = VrmlData_IrrelevantNumber;
+                    break;
+                }
+        }
+        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "diffuseColor")) {
+            if (OK(aStatus, Scene().ReadXYZ(theBuffer, aColor[0],
+                Standard_False, Standard_False)))
+                if (aColor[0].X() < -aConf || aColor[0].X() > 1. + aConf ||
+                    aColor[0].Y() < -aConf || aColor[0].Y() > 1. + aConf ||
+                    aColor[0].Z() < -aConf || aColor[0].Z() > 1. + aConf)
+                {
+                    aStatus = VrmlData_IrrelevantNumber;
+                    break;
+                }
+        }
+        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "emissiveColor")) {
+            if (OK(aStatus, Scene().ReadXYZ(theBuffer, aColor[1],
+                Standard_False, Standard_False)))
+                if (aColor[1].X() < -aConf || aColor[1].X() > 1. + aConf ||
+                    aColor[1].Y() < -aConf || aColor[1].Y() > 1. + aConf ||
+                    aColor[1].Z() < -aConf || aColor[1].Z() > 1. + aConf)
+                {
+                    aStatus = VrmlData_IrrelevantNumber;
+                    break;
+                }
+        }
+        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "specularColor")) {
+            if (OK(aStatus, Scene().ReadXYZ(theBuffer, aColor[2],
+                Standard_False, Standard_False)))
+                if (aColor[2].X() < -aConf || aColor[2].X() > 1. + aConf ||
+                    aColor[2].Y() < -aConf || aColor[2].Y() > 1. + aConf ||
+                    aColor[2].Z() < -aConf || aColor[2].Z() > 1. + aConf)
+                {
+                    aStatus = VrmlData_IrrelevantNumber;
+                    break;
+                }
         }
         else
             break;
+
         if (!OK(aStatus))
             break;
     }
+
     // Read the terminating (closing) brace
-    if (OK(aStatus) || aStatus == VrmlData_EmptyData)
-        if (OK(aStatus, readBrace(theBuffer))) {
-            // Post-processing
-            ;
-        }
+    if (OK(aStatus))
+        aStatus = readBrace(theBuffer);
+
+    // Store the values in the Material node instance
+    if (OK(aStatus)) {
+        myAmbientIntensity = anIntensity[0];
+        myShininess = anIntensity[1];
+        myTransparency = anIntensity[2];
+        myDiffuseColor.SetValues(aColor[0].X(), aColor[0].Y(), aColor[0].Z(),
+            Quantity_TOC_sRGB);
+        myEmissiveColor.SetValues(aColor[1].X(), aColor[1].Y(), aColor[1].Z(),
+            Quantity_TOC_sRGB);
+        mySpecularColor.SetValues(aColor[2].X(), aColor[2].Y(), aColor[2].Z(),
+            Quantity_TOC_sRGB);
+    }
     return aStatus;
 }
 
 //=======================================================================
-//function : Write
-//purpose  : Write the Node to output stream
+//function : VrmlData_Material::Write
+//purpose  : 
 //=======================================================================
 
-VrmlData_ErrorStatus VrmlData_IndexedLineSet::Write
-(const char* thePrefix) const
+VrmlData_ErrorStatus VrmlData_Material::Write(const char* thePrefix) const
 {
-    static char header[] = "IndexedLineSet {";
+    VrmlData_ErrorStatus aStatus = VrmlData_StatusOK;
     const VrmlData_Scene& aScene = Scene();
-    VrmlData_ErrorStatus aStatus;
-    if (OK(aStatus, aScene.WriteLine(thePrefix, header, GlobalIndent()))) {
+    static char header[] = "Material {";
+    if (aScene.IsDummyWrite() == Standard_False &&
+        OK(aStatus, aScene.WriteLine(thePrefix, header, GlobalIndent())))
+    {
+        char buf[128];
+        Standard_Real val[3];
+        const Quantity_TypeOfColor bidType = Quantity_TOC_sRGB;
+        const Standard_Real aConf(0.001 * Precision::Confusion());
 
-        if (OK(aStatus) && myCoords.IsNull() == Standard_False)
-            aStatus = aScene.WriteNode("coord", myCoords);
-        if (OK(aStatus))
-            aStatus = aScene.WriteArrIndex("coordIndex", myArrPolygons, myNbPolygons);
-
-        if (OK(aStatus) && myColorPerVertex == Standard_False)
-            aStatus = aScene.WriteLine("colorPerVertex  FALSE");
-        if (OK(aStatus) && myColors.IsNull() == Standard_False)
-            aStatus = aScene.WriteNode("color", myColors);
-        if (OK(aStatus))
-            aStatus = aScene.WriteArrIndex("colorIndex", myArrColorInd, myNbColors);
+        if (OK(aStatus) && fabs(myAmbientIntensity - 0.2) > aConf) {
+            Sprintf(buf, "%.6g", myAmbientIntensity);
+            aStatus = aScene.WriteLine("ambientIntensity ", buf);
+        }
+        if (OK(aStatus)) {
+            myDiffuseColor.Values(val[0], val[1], val[2], bidType);
+            if ((val[0] - 0.8) * (val[0] - 0.8) +
+                (val[1] - 0.8) * (val[1] - 0.8) +
+                (val[2] - 0.8) * (val[2] - 0.8) > 1e-7)
+            {
+                Sprintf(buf, "%.6g %.6g %.6g", val[0], val[1], val[2]);
+                aStatus = aScene.WriteLine("diffuseColor     ", buf);
+            }
+        }
+        if (OK(aStatus)) {
+            myEmissiveColor.Values(val[0], val[1], val[2], bidType);
+            if (val[0] * val[0] + val[1] * val[1] + val[2] * val[2] > 1e-7) {
+                Sprintf(buf, "%.6g %.6g %.6g", val[0], val[1], val[2]);
+                aStatus = aScene.WriteLine("emissiveColor    ", buf);
+            }
+        }
+        if (OK(aStatus) && fabs(myShininess - 0.2) > aConf) {
+            Sprintf(buf, "%.6g", myShininess);
+            aStatus = aScene.WriteLine("shininess        ", buf);
+        }
+        if (OK(aStatus)) {
+            mySpecularColor.Values(val[0], val[1], val[2], bidType);
+            if (val[0] * val[0] + val[1] * val[1] + val[2] * val[2] > 1e-7) {
+                Sprintf(buf, "%.6g %.6g %.6g", val[0], val[1], val[2]);
+                aStatus = aScene.WriteLine("specularColor    ", buf);
+            }
+        }
+        if (OK(aStatus) && myTransparency > aConf) {
+            Sprintf(buf, "%.6g", myTransparency);
+            aStatus = aScene.WriteLine("transparency     ", buf);
+        }
 
         aStatus = WriteClosing();
     }
@@ -204,17 +248,33 @@ VrmlData_ErrorStatus VrmlData_IndexedLineSet::Write
 
 //=======================================================================
 //function : IsDefault
-//purpose  : Returns True if the node is default,
-//           so that it should not be written.
+//purpose  : 
 //=======================================================================
 
-Standard_Boolean VrmlData_IndexedLineSet::IsDefault() const
+Standard_Boolean VrmlData_Material::IsDefault() const
 {
-    Standard_Boolean aResult(Standard_True);
-    if (myNbPolygons)
-        aResult = Standard_False;
-    else if (myCoords.IsNull() == Standard_False)
-        aResult = myCoords->IsDefault();
+    const Standard_Real aConf(0.001 * Precision::Confusion());
+    Standard_Boolean aResult(Standard_False);
+    if (fabs(myAmbientIntensity - 0.2) < aConf &&
+        fabs(myShininess - 0.2) < aConf &&
+        myTransparency < aConf)
+    {
+        Standard_Real val[3][3];
+        const Quantity_TypeOfColor bidType = Quantity_TOC_sRGB;
+        myDiffuseColor.Values(val[0][0], val[0][1], val[0][2], bidType);
+        myEmissiveColor.Values(val[1][0], val[1][1], val[1][2], bidType);
+        mySpecularColor.Values(val[2][0], val[2][1], val[2][2], bidType);
+        aResult = (((val[0][0] - 0.8) * (val[0][0] - 0.8) +
+            (val[0][1] - 0.8) * (val[0][1] - 0.8) +
+            (val[0][2] - 0.8) * (val[0][2] - 0.8) < 1e-7) &&
+            (val[1][0] * val[1][0] +
+                val[1][1] * val[1][0] +
+                val[1][2] * val[1][0] < 1e-7) &&
+            (val[2][0] * val[2][0] +
+                val[2][1] * val[2][0] +
+                val[2][2] * val[2][0] < 1e-7));
+    }
     return aResult;
 }
+
 
