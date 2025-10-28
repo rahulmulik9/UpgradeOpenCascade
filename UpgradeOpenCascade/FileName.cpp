065c95447e620a53f3a839cc2198d01e6016633f
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2025-10-28 18:14:56 +0530

    d

--- UpgradeOpenCascade/FileName.cpp
+++ UpgradeOpenCascade/FileName.cpp
@@ -21,14 +21,10 @@ IMPLEMENT_STANDARD_RTTIEXT(VrmlData_WorldInfo, VrmlData_Node)
 
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_DEPRECATE
-#pragma warning (disable:4996)
+#pragma warning(disable : 4996)
 #endif
 
-
-//=======================================================================
-//function : VrmlData_WorldInfo::VrmlData_WorldInfo
-//purpose  : Constructor
-//=======================================================================
+//=================================================================================================
 
 VrmlData_WorldInfo::VrmlData_WorldInfo(const VrmlData_Scene& theScene,
     const char* theName,
@@ -40,63 +36,65 @@ VrmlData_WorldInfo::VrmlData_WorldInfo(const VrmlData_Scene& theScene,
 }
 
 //=======================================================================
-//function : SetTitle
-//purpose  : Set or modify the title.
+// function : SetTitle
+// purpose  : Set or modify the title.
 //=======================================================================
 
 void VrmlData_WorldInfo::SetTitle(const char* theString)
 {
     if (theString == 0L)
         myTitle = 0L;
-    else {
+    else
+    {
         const size_t len = strlen(theString) + 1;
         if (len == 1)
             myTitle = 0L;
-        else {
-            myTitle = static_cast <const char*>(Scene().Allocator()->Allocate(len));
-            memcpy(const_cast<char*> (myTitle), theString, len);
+        else
+        {
+            myTitle = static_cast<const char*>(Scene().Allocator()->Allocate(len));
+            memcpy(const_cast<char*>(myTitle), theString, len);
         }
     }
 }
 
 //=======================================================================
-//function : AddInfo
-//purpose  : Add a string to the list of info strings.
+// function : AddInfo
+// purpose  : Add a string to the list of info strings.
 //=======================================================================
 
 void VrmlData_WorldInfo::AddInfo(const char* theString)
 {
     if (theString != 0L)
-        if (*theString != '\0') {
+        if (*theString != '\0')
+        {
             const size_t len = strlen(theString) + 1;
-            char* aStr = static_cast <char*>(Scene().Allocator()->Allocate(len));
+            char* aStr = static_cast<char*>(Scene().Allocator()->Allocate(len));
             memcpy(aStr, theString, len);
             myInfo.Append(aStr);
         }
 }
 
 //=======================================================================
-//function : Clone
-//purpose  : Create a copy of this node
+// function : Clone
+// purpose  : Create a copy of this node
 //=======================================================================
 
-Handle(VrmlData_Node) VrmlData_WorldInfo::Clone
-(const Handle(VrmlData_Node)& theOther) const
+Handle(VrmlData_Node) VrmlData_WorldInfo::Clone(const Handle(VrmlData_Node)& theOther) const
 {
     Handle(VrmlData_WorldInfo) aResult =
         Handle(VrmlData_WorldInfo)::DownCast(VrmlData_Node::Clone(theOther));
     if (aResult.IsNull())
-        aResult =
-        new VrmlData_WorldInfo(theOther.IsNull() ? Scene() : theOther->Scene(),
-            Name());
+        aResult = new VrmlData_WorldInfo(theOther.IsNull() ? Scene() : theOther->Scene(), Name());
 
-    if (&aResult->Scene() == &Scene()) {
+    if (&aResult->Scene() == &Scene())
+    {
         aResult->myTitle = myTitle;
         aResult->myInfo = myInfo;
     }
-    else {
+    else
+    {
         aResult->SetTitle(myTitle);
-        NCollection_List <const char*>::Iterator anIter(myInfo);
+        NCollection_List<const char*>::Iterator anIter(myInfo);
         for (; anIter.More(); anIter.Next())
             aResult->AddInfo(anIter.Value());
     }
@@ -104,24 +102,27 @@ Handle(VrmlData_Node) VrmlData_WorldInfo::Clone
 }
 
 //=======================================================================
-//function : Read
-//purpose  : Read the Node from input stream.
+// function : Read
+// purpose  : Read the Node from input stream.
 //=======================================================================
 
 VrmlData_ErrorStatus VrmlData_WorldInfo::Read(VrmlData_InBuffer& theBuffer)
 {
     VrmlData_ErrorStatus aStatus;
-    while (OK(aStatus, VrmlData_Scene::ReadLine(theBuffer))) {
+    while (OK(aStatus, VrmlData_Scene::ReadLine(theBuffer)))
+    {
 
-        if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "title")) {
+        if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "title"))
+        {
             TCollection_AsciiString aTitleString;
             if (OK(aStatus, ReadString(theBuffer, aTitleString)))
                 SetTitle(aTitleString.ToCString());
-
         }
-        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "info")) {
+        else if (VRMLDATA_LCOMPARE(theBuffer.LinePtr, "info"))
+        {
             NCollection_List<TCollection_AsciiString> lstInfo;
-            if (OK(aStatus, ReadMultiString(theBuffer, lstInfo))) {
+            if (OK(aStatus, ReadMultiString(theBuffer, lstInfo)))
+            {
                 NCollection_List<TCollection_AsciiString>::Iterator anIter(lstInfo);
                 for (; anIter.More(); anIter.Next())
                     AddInfo(anIter.Value().ToCString());
@@ -138,28 +139,32 @@ VrmlData_ErrorStatus VrmlData_WorldInfo::Read(VrmlData_InBuffer& theBuffer)
 }
 
 //=======================================================================
-//function : Write
-//purpose  : Write the Node to the Scene output.
+// function : Write
+// purpose  : Write the Node to the Scene output.
 //=======================================================================
 
 VrmlData_ErrorStatus VrmlData_WorldInfo::Write(const char* thePrefix) const
 {
-    VrmlData_ErrorStatus aStatus(VrmlData_StatusOK);
+    VrmlData_ErrorStatus  aStatus(VrmlData_StatusOK);
     const VrmlData_Scene& aScene = Scene();
-    static char header[] = "WorldInfo {";
-    if (aScene.IsDummyWrite() == Standard_False &&
-        OK(aStatus, aScene.WriteLine(thePrefix, header, GlobalIndent())))
+    static char           header[] = "WorldInfo {";
+    if (aScene.IsDummyWrite() == Standard_False
+        && OK(aStatus, aScene.WriteLine(thePrefix, header, GlobalIndent())))
     {
         char buf[4096];
-        if (myTitle) {
+        if (myTitle)
+        {
             Sprintf(buf, "title \"%s\"", myTitle);
             aStatus = aScene.WriteLine(buf);
         }
 
-        if (myInfo.IsEmpty() == Standard_False && OK(aStatus)) {
-            if (OK(aStatus, aScene.WriteLine("info [", 0L, GlobalIndent()))) {
+        if (myInfo.IsEmpty() == Standard_False && OK(aStatus))
+        {
+            if (OK(aStatus, aScene.WriteLine("info [", 0L, GlobalIndent())))
+            {
                 NCollection_List<const char*>::Iterator anIter(myInfo);
-                while (anIter.More()) {
+                while (anIter.More())
+                {
                     Sprintf(buf, "\"%s\"", anIter.Value());
                     anIter.Next();
                     if (anIter.More())
@@ -176,10 +181,7 @@ VrmlData_ErrorStatus VrmlData_WorldInfo::Write(const char* thePrefix) const
     return aStatus;
 }
 
-//=======================================================================
-//function : IsDefault
-//purpose  : 
-//=======================================================================
+//=================================================================================================
 
 Standard_Boolean VrmlData_WorldInfo::IsDefault() const
 {
